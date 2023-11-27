a647085562abb9f23ba2ea71333396de1f0a3635
klendathu2k <jwebb@bnl.gov>
2023-11-27 11:25:50 -0500

    ... hopefully working submission setup ...

--- submit_calor.py
+++ submit_calor.py
@@ -9,8 +9,8 @@ from slurp import matches
 from slurp import submit
 
 indir ="/sphenix/lustre01/sphnxpro/commissioning/aligned_2Gprdf/"
-#outdir="/sphenix/lustre01/sphnxpro/slurp/"
-outdir="/sphenix/u/jwebb2/work/2023/slurp/out/"
+outdir="/sphenix/lustre01/sphnxpro/slurp/"
+#outdir="/sphenix/u/jwebb2/work/2023/slurp/out/"
 
 DST_CALOR_query = """
 select filename,runnumber,segment from datasets
@@ -25,6 +25,8 @@ select filename,runnumber,segment from datasets
 job=Job(
     executable="/sphenix/u/jwebb2/work/2023/slurp/MDC2/submit/rawdata/caloreco/rundir/run_caloreco.sh",
     initialdir="/sphenix/u/jwebb2/work/2023/slurp/MDC2/submit/rawdata/caloreco/rundir/"
+    output_destination="file:///sphenix/u/jwebb2/work/2023/slurp/output/",
+    transfer_output_files="$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).log,$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).errlog"
     )
 
 
@@ -36,4 +38,4 @@ DST_CALOR_rule = Rule( name              = "DST_CALOR_auau23",
                        tag               = "2023p003",
                        job               = job)
 
-submit(DST_CALOR_rule, nevents=2, indir=indir, outdir=outdir, condor=".", dump=False )  
+submit(DST_CALOR_rule, nevents=10, indir=indir, outdir=outdir, dump=False, resubmit=True )  
