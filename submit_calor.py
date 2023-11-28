a6ad186bc796ec412f817b81266978cb5352fb43
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-11-27 19:48:55 -0500

    ... condor files and log files to same location... output files directed to subdirectories in lustre ...

--- submit_calor.py
+++ submit_calor.py
@@ -8,26 +8,28 @@ from slurp import SPhnxCondorJob as Job
 from slurp import matches
 from slurp import submit
 
-indir ="/sphenix/lustre01/sphnxpro/commissioning/aligned_2Gprdf/"
-outdir="/sphenix/lustre01/sphnxpro/slurp/"
-#outdir="/sphenix/u/jwebb2/work/2023/slurp/out/"
+indir  = "/sphenix/lustre01/sphnxpro/commissioning/aligned_2Gprdf/"
+#outdir = "/sphenix/lustre01/sphnxpro/slurp/"
+outdir = "/sphenix/lustre01/sphnxpro/slurp/$$([$(run)/100])00"
+logdir = "file:///sphenix/data/data02/sphnxpro/condorlogs/$$([$(run)/100])00"
+condor = logdir.replace("file://","") 
 
 DST_CALOR_query = """
 select filename,runnumber,segment from datasets
    where dsttype = 'beam'
    and filename like 'beam-%'
-   and runnumber > 22000
+   and runnumber = 22026
    order by runnumber,segment
-   limit 5
+   limit 10
 """
+
 
 
 job=Job(
     executable            = "/sphenix/u/sphnxpro/slurp/MDC2/submit/rawdata/caloreco/rundir/run_caloreco.sh",
-    initialdir            = "/sphenix/u/sphnxpro/slurp/MDC2/submit/rawdata/caloreco/rundir/",
-    #output_destination:    str = "file:////sphenix/data/data02/sphnxpro/condorlog/$$($(run)/100)00"         
-    output_destination    = "file:///sphenix/data/data02/sphnxpro/condorlogs/$$([$(run)/100])00",
-    transfer_output_files = "$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).log,$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).errlog",
+#   initialdir            = "/sphenix/u/sphnxpro/slurp/MDC2/submit/rawdata/caloreco/rundir/",
+    output_destination    = logdir,
+#   transfer_output_files = "$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).log,$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).errlog",
     transfer_input_files  = "/sphenix/u/sphnxpro/slurp/MDC2/submit/rawdata/caloreco/rundir/",
     )
 
@@ -40,4 +42,4 @@ DST_CALOR_rule = Rule( name              = "DST_CALOR_auau23",
                        tag               = "2023p003",
                        job               = job)
 
-submit(DST_CALOR_rule, nevents=10, indir=indir, outdir=outdir, dump=False, resubmit=True )  
+submit(DST_CALOR_rule, nevents=10, indir=indir, outdir=outdir, dump=False, resubmit=True, condor=condor ) 
