fa47625d47ead01b80965a3765957b4210859cc8
klendathu2k <jwebb@bnl.gov>
2023-12-14 15:51:28 -0500

    iterating on the event builder workflow

--- kaedama.py
+++ kaedama.py
@@ -14,8 +14,10 @@ import pprint
 
 # Extend the command line arguments
 arg_parser.add_argument( '-n', '--nevents', default=0, dest='nevents', help='Number of events to process.  0=all.', type=int)
-arg_parser.add_argument( '--rule', help="Submit against specified rule", default="DST_CALOR" )
+arg_parser.add_argument( '--rule', help="Submit against specified rule", default="DST_EVENT" )
 arg_parser.add_argument( '--limit', help="Maximum number of jobs to submit", default=0, type=int )
+arg_parser.add_argument( '--submit',help="Job will be submitted", dest="submit", default="True", action="store_true")
+arg_parser.add_argument( '--no-submit', help="Job will not be submitted... print things", dest="submit", action="store_false")
 
 # parse command line options
 args = slurp.parse_command_line()
@@ -40,7 +42,7 @@ if args.rule == "DST_CALOR":
         executable            = "/sphenix/u/sphnxpro/slurp/MDC2/submit/rawdata/caloreco/rundir/run_caloreco.sh",
         output_destination    = logdir,
         transfer_output_files = "$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).out,$(name)_$(build)_$(tag)-$INT(run,%08d)-$INT(seg,%04d).err",
-        transfer_input_files  = "$(payload),cups.py"
+        transfer_input_files  = "$(payload),cups.py",
     )
 
 
@@ -57,3 +59,76 @@ if args.rule == "DST_CALOR":
 
     submit(DST_CALOR_rule, nevents=args.nevents, indir=indir, outdir=outdir, dump=False, resubmit=True, condor=condor ) 
 
+
+elif args.rule == 'DST_EVENT':
+
+    DST_EVENT_query = """
+    select DISTINCT ON (runnumber) filename,runnumber,segment from datasets 
+    where ( filename like 'beam_seb%' or filename like 'beam_East%' or filename like 'beam_West%' or filename like 'beam_LL1%' or filename like 'GL1_beam%') 
+    and runnumber = 22026
+    order by runnumber,segment
+    """
+
+    file_lists = ','.join("""
+    eventcombine/lists/hcaleast_$(run).list
+    eventcombine/lists/hcalwest_$(run).list
+    eventcombine/lists/ll1_$(run).list
+    eventcombine/lists/mbd_$(run).list
+    eventcombine/lists/seb00_$(run).list
+    eventcombine/lists/seb01_$(run).list
+    eventcombine/lists/seb02_$(run).list
+    eventcombine/lists/seb03_$(run).list
+    eventcombine/lists/seb04_$(run).list
+    eventcombine/lists/seb05_$(run).list
+    eventcombine/lists/seb06_$(run).list
+    eventcombine/lists/seb07_$(run).list
+    eventcombine/lists/zdc_$(run).list""".replace(' ','').split('\n'))
+
+    #cups_cmd = f"-d DST_EVENT_auau23 -r $(run) -s $(seg) execute $(script) $(nevents) $(name)_$(build)_$(tag) $(run) $(ClusterId) $(ProcId)"
+
+    logbase = "$(name)_$(build)_$(tag)-$INT(run,%08d)-0000"
+    outbase = "$(name)_$(build)_$(tag)"
+    script_cmd = f"$(nevents) {outbase} {logbase} {outdir} $(run) $(ClusterId) $(ProcId) $(build) $(tag)"
+
+    Xjob=Job(
+        executable            = "/sphenix/u/sphnxpro/slurp/MDC2/submit/rawdata/caloreco/rundir/hello_world.sh",
+        arguments             =  script_cmd,
+        #executable            = "cups.py",
+        #arguments             = cups_cmd,
+        output_destination    = f"{logdir}",
+        #transfer_output_files = f"{logbase}.out,{logbase}.err",
+        #transfer_output_files = "hello_world.sh",
+        transfer_input_files  =  "$(payload),cups.py,"+file_lists,
+        output                = f'{logbase}.condor.stdout',
+        error                 = f'{logbase}.condor.stderr',
+        log                   = f'$(condor)/{logbase}.condor',
+    )
+
+    job=Job(
+        executable            = "/sphenix/u/sphnxpro/slurp/eventcombine/run.csh",
+        arguments             =  script_cmd,
+        #executable            = "cups.py",
+        #arguments             = cups_cmd,
+        output_destination    = f"{logdir}",
+        #transfer_output_files = f"{logbase}.out,{logbase}.err",
+        #transfer_output_files = "hello_world.sh",
+        transfer_input_files  =  "$(payload),cups.py,"+file_lists,
+        output                = f'{logbase}.condor.stdout',
+        error                 = f'{logbase}.condor.stderr',
+        log                   = f'$(condor)/{logbase}.condor',
+    )
+
+    if args.submit:
+        DST_EVENT_rule = Rule( name   = "DST_EVENT_auau23",
+                               files  = DST_EVENT_query,
+                               script = "run.csh",
+                               build  = "ana.393",
+                               tag    = "2023p009",
+                               payload = "/sphenix/u/sphnxpro/slurp/eventcombine/",
+                               job    = job,
+                               limit  = args.limit )
+
+        submit(DST_EVENT_rule, nevents=args.nevents, indir=indir, outdir=outdir, dump=False, resubmit=True, condor=condor ) 
+
+    else:
+        pprint.pprint(job)
