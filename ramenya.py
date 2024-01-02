8e775f1f7b6e30b6afed70a28fa9e9c1bdd1d2c6
klendathu2k <jwebb@bnl.gov>
2024-01-02 13:19:26 -0500

    Specify runs (single, range or list) on ramenya command line.

--- ramenya.py
+++ ramenya.py
@@ -3,10 +3,17 @@ import sh
 from time import sleep
 from contextlib import redirect_stdout
 import sys
+import argparse
+
 
 kaedama  = sh.Command("kaedama.py")
 condor_q = sh.Command("condor_q")
 psql     = sh.Command("psql")
+
+arg_parser = argparse.ArgumentParser()    
+arg_parser.add_argument( '--runs', nargs='+', help="One argument for a specific run.  Two arguments an inclusive range.  Three or more, a list", default=['26022'] )
+args = arg_parser.parse_args()
+
 
 
 def main():
@@ -14,21 +21,31 @@ def main():
     while (True):
         print("Running the DST_EVENT rule")
 
-        kaedama( "--runs", "22026", "22027", 
-                 rule="DST_EVENT", 
-                 batch=True, 
-                 _out=sys.stdout )
+        if len(args.runs)==1:
+            kaedama( runs=args.runs[0], rule="DST_EVENT", batch=True, _out=sys.stdout )
+        elif len(args.runs)==2:
+            kaedama( "--runs", args.runs[0], args.runs[1], rule="DST_EVENT", batch=True, _out=sys.stdout )
+        elif len(args.runs)>2:
+            for r in args.runs:
+                kaedama( "--runs", r, rule="DST_EVENT", batch=True, _out=sys.stdout )
 
 
         print("Running the DST_CALOR rule")
-        kaedama( 
-            "--runs", "22026", "22027",
-            rule="DST_CALOR", batch=True )
+
+        if len(args.runs)==1:
+            kaedama( runs=args.runs[0], rule="DST_CALOR", batch=True, _out=sys.stdout )
+        elif len(args.runs)==2:
+            kaedama( "--runs", args.runs[0], args.runs[1], rule="DST_CALOR", batch=True, _out=sys.stdout )
+        elif len(args.runs)>2:
+            for r in args.runs:
+                kaedama( "--runs", r, rule="DST_CALOR", batch=True, _out=sys.stdout )
+
+
         condor_q("-batch","sphnxpro",_out=sys.stdout)        
 
 
         psql(dbname="FileCatalog", 
-             command="select dsttype,run,segment,cluster,process,status,started,running,ended,exit_code from production_status where run<=22027 and run>=22026 order by id;", _out=sys.stdout);
+             command="select dsttype,run,segment,cluster,process,status,started,running,ended,exit_code from production_status order by id desc limit 20;", _out=sys.stdout);
 
 
         print("Pausing loop for 2min")
