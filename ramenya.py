bfb43ea230e3982d46fad66e16dbc8c15eb28bd8
klendathu2k <jwebb@bnl.gov>
2024-01-19 13:25:08 -0500

    Specify config file.  Aggregate table by runnumber.

--- ramenya.py
+++ ramenya.py
@@ -18,10 +18,9 @@ arg_parser.add_argument( '--submit', help="Submit jobs to condor",default=True,a
 arg_parser.add_argument( '--no-submit', help="No submission, just print the summary information",action="store_false",dest="submit")
 arg_parser.add_argument( '--outputs',help="Information printed at each loop",nargs='+', default=['started'] )
 arg_parser.add_argument( '--once',help="Break out of the loop after one iteration",default=False,action="store_true")
+arg_parser.add_argument( '--config',help="Specifies the configuration file used by kaedama to submit workflows", default='sphenix_auau23.yaml')
 
 args = arg_parser.parse_args()
-
-
 
 def main():
     
@@ -32,22 +31,22 @@ def main():
             if 'all' in args.rules or 'DST_EVENT' in args.rules:
                 print("Running the DST_EVENT rule")
                 if len(args.runs)==1:
-                    kaedama( runs=args.runs[0], rule="DST_EVENT", config="sphenix_auau23.yaml", batch=True, _out=sys.stdout )
+                    kaedama( runs=args.runs[0], rule="DST_EVENT", config=args.config, batch=True, _out=sys.stdout )
                 elif len(args.runs)==2:
-                    kaedama( "--runs", args.runs[0], args.runs[1], rule="DST_EVENT", config="sphenix_auau23.yaml", batch=True, _out=sys.stdout )
+                    kaedama( "--runs", args.runs[0], args.runs[1], rule="DST_EVENT", config=args.config, batch=True, _out=sys.stdout )
                 elif len(args.runs)>2:
                     for r in args.runs:
-                        kaedama( "--runs", r, rule="DST_EVENT", config="sphenix_auau23.yaml", batch=True, _out=sys.stdout )
+                        kaedama( "--runs", r, rule="DST_EVENT", config=args.config, batch=True, _out=sys.stdout )
 
             if 'all' in args.rules or 'DST_CALOR' in args.rules:
                 print("Running the DST_CALOR rule")
                 if len(args.runs)==1:
-                    kaedama( runs=args.runs[0], rule="DST_CALOR", config="sphenix_auau23.yaml", batch=True, _out=sys.stdout )
+                    kaedama( runs=args.runs[0], rule="DST_CALOR", config=args.config, batch=True, _out=sys.stdout )
                 elif len(args.runs)==2:
-                    kaedama( "--runs", args.runs[0], args.runs[1], rule="DST_CALOR", config="sphenix_auau23.yaml", batch=True, _out=sys.stdout )
+                    kaedama( "--runs", args.runs[0], args.runs[1], rule="DST_CALOR", config=args.config, batch=True, _out=sys.stdout )
                 elif len(args.runs)>2:
                     for r in args.runs:
-                        kaedama( "--runs", r, rule="DST_CALOR", config="sphenix_auau23.yaml", batch=True, _out=sys.stdout )
+                        kaedama( "--runs", r, rule="DST_CALOR", config=args.config, batch=True, _out=sys.stdout )
 
 
         if 'condorq' in args.outputs:
@@ -77,6 +76,7 @@ def main():
             print("--------------------------------------------------")
             psqlquery="""
             select dsttype,
+               run,
                count(run)                      as num_jobs,
                avg(age(started,submitting))    as avg_time_to_start,
                count( case status when 'submitted' then 1 else null end )
@@ -94,8 +94,8 @@ def main():
        
             from   production_status 
             where  status>='started' 
-            group by dsttype
-            order by dsttype desc
+            group by dsttype,run
+            order by dsttype,run desc
                ;
             """
             psql(dbname="FileCatalog",command=psqlquery,_out=sys.stdout)
