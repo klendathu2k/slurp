9c428322bac16fccb65193f5a7a4b1cd35cb2251
klendathu2k <jwebb@bnl.gov>
2024-05-28 18:54:54 -0400

    Support for both the physics and commissioning periods.

--- ramenya2.py
+++ ramenya2.py
@@ -405,6 +405,7 @@ def query_jobs_by_condor(conditions="", title="Summary of jobs by with condor st
     argument( "--rules-file", dest="rules_file", default=None, help="If specified, read the list of active rules from the given file on each pass of the loop" ),
     argument( "--timestart",default=datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0),help="Specifies UTC timestamp (in ISO format, e.g. YYYY-MM-DD) for query.", type=dateutil.parser.parse),
     argument( "--test",default=False,help=argparse.SUPPRESS,action="store_true"), # kaedama will be submitted in batch mode
+    argument( "--experiment-mode", default=None, help="Sets experiment-mode for kaedama", dest="mode" ),
     argument( "SLURPFILE",   help="Specifies the slurpfile(s) containing the job definitions" )
 ])
 def submit(args):
@@ -419,6 +420,8 @@ def submit(args):
     kaedama = kaedama.bake( "submit", "--config", args.SLURPFILE, "--nevents", args.nevents )
     if args.test:
         kaedama = kaedama.bake( "--batch" )
+    if args.mode is not None:
+        kaedama = kaedama.bake( "--experiment-mode", args.mode )
 
     while ( go ):
 
