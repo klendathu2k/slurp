2c9358a9eed16bdca927b0e08c7e31bd6e8bcfc7
klendathu2k <jwebb@bnl.gov>
2024-09-04 12:02:44 -0400

    Create production status entries with state 'submitting' prior to the submission via condor.

--- slurp.py
+++ slurp.py
@@ -639,7 +639,9 @@ def submit( rule, maxjobs, **kwargs ):
                 
         run_submit_loop=30
         schedd_query = None
-
+        
+        INFO("... insert")
+        insert_production_status( matching, setup, [], state="submitting" ) 
 
         INFO("Submitting the jobs to the cluster")
         submit_result = schedd.submit(submit_job, itemdata=iter(mymatching))  # submit one job for each item in the itemdata
@@ -653,8 +655,6 @@ def submit( rule, maxjobs, **kwargs ):
         INFO("Insert and update the production_status")
         if ( schedd_query ):
             
-            INFO("... insert")
-            insert_production_status( matching, setup, schedd_query, state="submitted" ) 
 
             INFO("... result")
             result = submit_result.cluster()
