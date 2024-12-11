76766d4a1d43d67a51ae3948efda54c141216e82
klendathu2k <jwebb@bnl.gov>
2024-12-11 17:51:11 -0500

    Cups will need to run system python rather than sphenix python under alma9 to get all dependencies.

--- jobwrapper.sh
+++ jobwrapper.sh
@@ -4,7 +4,6 @@
 hostname
 echo $@
 
-
 export userscript=$1       # this is the executable script
 export cupsid=${@: -1:1}                         # this is the ID of the job on the production system
 export payload=( `echo ${@: -2:1} | tr ","  " "` ) # comma sep list --> array of files to stage in
@@ -14,9 +13,26 @@ myArgs=( "$@")
 shift
 userArgs=( "$@" )
 
+OS=$( hostnamectl | awk '/Operating System/{ print $3" "$4 }' )
+echo "Setting up SLURP for ${OS}"
+
 source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+export PATH=${PATH}:${HOME}/bin
+
+if [[ $OS =~ "Alma" ]]; then
+   echo "Rejiggering python path"
+   export PYTHONPATH=/opt/sphenix/core/lib/python3.9/site-packages
+   alias python=/usr/bin/python
+   alias pip=/opt/sphenix/core/bin/pip3.9
+fi
+
 echo Argument list is now completely fubared
 echo $@
+
+# Job wrapper will start with sphenix_setup level at NEW
+#source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+
+
 echo userscript: ${userscript}
 echo cupsid:     ${cupsid}
 echo payload:    ${payload[@]}
@@ -26,7 +42,6 @@ echo subdir:     ${subdir}
 for i in ${payload[@]}; do
     cp --verbose $subdir/$i . 
 done
-
 
 chmod u+x ${userscript}
 
