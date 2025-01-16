cdd3dd32d54551cde8f8dc4373f89f555119fdac
klendathu2k <jwebb@bnl.gov>
2025-01-16 09:57:51 -0500

    Remove unused fccro dependency.

--- kaedama.py
+++ kaedama.py
@@ -13,7 +13,6 @@ from slurp import matches
 from slurp import submit
 
 from slurp import arg_parser
-from slurp import fccro as fcc  # production status DB cursor
 from slurp import daqc 
 
 #from slurp import RUNFMT
